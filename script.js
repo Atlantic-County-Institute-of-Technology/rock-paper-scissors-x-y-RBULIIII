a06e16fc3c06c8370d754bde3baeb5c41afff71e
RBULIIII <buli9990@student.acitech.org>
2025-03-20 09:14:20 -0400

    I added all my pictures. I just need to add my script to my java

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ buttons.addEventListener("click" , annoy);
 function annoy (){
     mouse.play();
 }
+
+if {player makes a choice
+rock > paper > scissors}
+
+Computer chooses random item 
+
+if {player chose rock & computer chose scissors
+player wins}
