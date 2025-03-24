80179e589f526cff03a5215be601e07a5e64fd62
RBULIIII <buli9990@student.acitech.org>
2025-03-24 09:12:57 -0400

    I added my a background for my website and started giving a functino to my buttons from java and I still need to add cpu choice checkwinner to make it work

--- script.js
+++ script.js
@@ -3,15 +3,50 @@ const Paper = document.getElementById("Paper")
 const Scissors = document.getElementById("Scissors")
 const button = document.getElementById("button")
 const button1 = document.getElementById("button1")
-let userPick = "";
+var userPick = 0;
 
 function userChoiceRock() {
     userPick = 1;
         alert(userPick)
 }
 
-Rock.addEventListener("click", userChoiceRock)
+Rock.addEventListener("click", () => {
+    userChoice(1);
+    // cpuChoice()
+    // checkWinner()
+})
 
+function userChoice(choice) {
+    userPick = choice;
+}
+
+function userChoicePaper() {
+    userPick = 2;
+        alert(userPick)
+}
+
+Paper.addEventListener("click", userChoicePaper)
+
+function userChoiceScissors() {
+    userPick = 3;
+        alert(userPick)
+}
+
+Scissors.addEventListener("click", userChoiceScissors)
+
+function userChoicebutton() {
+    userPick = 4;
+        alert(userPick)
+}
+
+button.addEventListener("click", userChoicebutton)
+
+function userChoicebutton1() {
+    userPick = 5;
+        alert(userPick)
+}
+
+button1.addEventListener("click", userChoicebutton1)
 
 // let mouse = new Audio('assests/mouse.mp3');
 
