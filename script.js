557abe8b62397d1decb5a19bb6253601c88b1294
RBULIIII <buli9990@student.acitech.org>
2025-03-26 09:13:40 -0400

    I added comments

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
 const Rock = document.getElementById("Rock")
 const Paper = document.getElementById("Paper")
 const Scissors = document.getElementById("Scissors")
+/*The const tells the js that there is a document and whatever it is named*/
 const button = document.getElementById("button")
 const button1 = document.getElementById("button1")
 let playerscore = document.getElementById("score1")
 let computerscore = document.getElementById("score2")
+/*The let links the js to the index*/
 var userPick = 0;
 
 function userChoiceRock() {
     userPick = 1;
     alert(userPick)
 }
-
+/*This shows the users choice and option*/
 function cpuChoice(){
     return Math.floor(Math.random()* 5) + 1;
     console.log(opponentchoice)
 }
+/*This shows the cpu choice*/
 
 Rock.addEventListener("click", () => {
     userChoice(1);
     cpuChoice()
     checkWinner()
     opponentchoice = cpuChoice()
-    
 })
 
 function userChoice(choice) {
     userPick = choice;
 }
+
+/*This is the userchoice function*/
 
 function userChoicePaper() {
     userPick = 2;
